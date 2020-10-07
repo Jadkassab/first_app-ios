fa53641ba4f28e39e2dbb0b7666aa7446c0bc6de
Jadkassab <Kassab_jad@hotmail.com>
2020-10-07 16:41:27 +0300

    added UILabel outlet

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var label_Title:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
